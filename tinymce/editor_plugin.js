e914a744574959ceb23475aee19b28010e09d014
Raymond Wainman <raywainman@gmail.com>
2013-05-07 14:00:47 -0600

    adding some documentation to the javascript initialization code to help decrypt some of the weird function parameters

--- tinymce/editor_plugin.js
+++ tinymce/editor_plugin.js
@@ -13,10 +13,32 @@
  * ************************************************************************ */
 
 (function() {
-    tinymce.create('tinymce.plugins.MathEditor', {
-        init : function(ed) {
+    tinymce.create('tinymce.plugins.MathEditorPlugin', {
+        /**
+         * Initializes the plugin, this will be executed after the plugin has been created.
+         * This call is done before the editor instance has finished it's initialization so use the onInit event
+         * of the editor instance to intercept that event.
+         *
+         * @param {tinymce.Editor} editor Editor instance that the plugin is initialized in.
+         * @param {string} url Absolute URL to where the plugin is located.
+         */
+        init : function(editor, url) {
 
         }
+
+        /**
+         * Returns information about the plugin as a name/value array.
+         * The current keys are longname, author, authorurl, infourl and version.
+         *
+         * @return {Object} Name/value array containing information about the plugin.
+         */
+        getInfo : function() {
+            return {
+                longname : 'Moodle Math Editor plugin',
+                author : 'Oohoo',
+                version : "1.0"
+            };
+        }
     });
-    tinymce.PluginManager.add('matheditor', tinymce.plugins.MathEditor);
+    tinymce.PluginManager.add('matheditor', tinymce.plugins.MathEditorPlugin);
 })();
