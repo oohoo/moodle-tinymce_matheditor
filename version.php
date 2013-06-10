08570a44f91ddc23bf8eff92a21122d5275ef838
Raymond Wainman <raywainman@gmail.com>
2013-06-10 11:39:19 -0600

    update version

--- version.php
+++ version.php
@@ -16,6 +16,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013060400;
+$plugin->version = 2013061000;
 $plugin->requires = 2013042600;
 $plugin->component = 'tinymce_matheditor';
